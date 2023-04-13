ce515804d65d3aecbcc2fa05d2f6c2947934a4b1
arsenicgit <s160207@psbbschools.edu.in>
2023-04-14 00:31:26 +0530

    big changes

--- vite-project/main.js
+++ vite-project/main.js
@@ -1,12 +1,16 @@
 import "./style.css";
-
 import * as t from "three";
-
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
 
-import { OrbitControls } from "three/addons/controls/OrbitControls.js";
-
 const scene = new t.Scene();
+const spaceTexture = new t.TextureLoader().load("omeganebula1.png");
+
+scene.background = spaceTexture;
+
+const starContainer = new t.Object3D();
+
+scene.add(starContainer);
 
 const camera = new t.PerspectiveCamera(
   30,
@@ -18,10 +22,8 @@ const camera = new t.PerspectiveCamera(
 const renderer = new t.WebGL1Renderer({
   canvas: document.querySelector("#bg"),
 });
-
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
-camera.position.setZ(100);
 
 const geometry = new t.CylinderGeometry(10, 10, 20, 100, 100, false, 90, 8);
 const material = new t.MeshStandardMaterial({
@@ -30,8 +32,6 @@ const material = new t.MeshStandardMaterial({
 
 const meshh = new t.Mesh(geometry, material);
 
-//scene.add(meshh);
-
 const pointLight = new t.PointLight(0xffffff);
 pointLight.position.set(10, 10, 10);
 
@@ -39,33 +39,62 @@ const ambientLight = new t.AmbientLight(0xffffff);
 
 const controls = new OrbitControls(camera, renderer.domElement);
 
-scene.add(pointLight, ambientLight);
+scene.add(pointLight, ambientLight, meshh);
+meshh.position.setZ(-100);
 
 function addStar() {
-  const geometry = new t.SphereGeometry(0.25, 10, 10);
-  const material = new t.MeshStandardMaterial({ color: 0xffffff });
+  const geometry = new t.SphereGeometry(1, 10, 10);
+  const material = new t.MeshStandardMaterial({
+    color:
+      "#" +
+      t.MathUtils.randInt(0, 9) +
+      t.MathUtils.randInt(0, 9) +
+      t.MathUtils.randInt(0, 9) +
+      t.MathUtils.randInt(0, 9) +
+      t.MathUtils.randInt(0, 9) +
+      t.MathUtils.randInt(0, 9),
+  });
   const star = new t.Mesh(geometry, material);
 
   const [x, y, z] = Array(3)
     .fill()
-    .map(() => t.MathUtils.randFloatSpread(100));
-
+    .map(() => t.MathUtils.randFloatSpread(1000));
   star.position.set(x, y, z);
-  scene.add(star);
+  starContainer.add(star);
 }
 
-const loader = new GLTFLoader();
+Array(2000).fill().forEach(addStar);
 
-loader.load("nothing.glb", function (gltf) {
-  scene.add(gltf.scene);
+const loader = new GLTFLoader();
+const phoneContainer = new t.Object3D();
+
+scene.add(phoneContainer);
+loader.load("phone.gltf", function (gltf) {
+  phoneContainer.add(gltf.scene);
 });
 
-Array(200).fill().forEach(addStar);
+phoneContainer.position.set(10, 0, 60);
+
+function moveCamera() {
+  const tt = document.body.getBoundingClientRect().top;
+  phoneContainer.rotation.x += 0.1;
+  camera.position.z = 100 + tt * -0.05;
+  camera.position.x = tt * -0.05;
+  camera.position.y = tt * -0.05;
+}
+
+document.body.onscroll = moveCamera;
 
 function animate() {
   requestAnimationFrame(animate);
+  meshh.rotation.x += 0.02;
+  starContainer.rotation.x += 0.0006;
   controls.update();
   renderer.render(scene, camera);
 }
 
 animate();
+
+window.addEventListener("unload", function (e) {
+  window.scrollTo(0, 0);
+});
